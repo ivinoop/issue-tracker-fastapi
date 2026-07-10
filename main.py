3e5f5a3ae4fafb602647db3f2336494ff3ea42f0
ivinoop <ivinoop@gmail.com>
2026-07-10 12:48:48 +0530

    cleanup

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app.middleware("http")(timing_middleware)
 app.add_middleware(
   CORSMiddleware,
   allow_origins=["*"],
-  allow_Credentials=True,
+  allow_credentials=True,
   allow_methods=["*"],
   allow_headers=["*"],
 )
